963f75d683c11d633bc18a86cb1bfe41b40cb1df
Sanyam Pandey <sanyam071104@gmail.com>
2024-09-15 08:28:03 +0530

    create main page

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Home' });
+    res.render('index', {sampleBlogs, highlightSection});
 });
 
 
 app.get('/blog', (req, res) => {
-    res.render('blog', { title: 'Blog Page', blogs: sampleBlogs });
+    res.render('blog', { title: 'Blog Page' });
 });
 
 
@@ -27,13 +27,25 @@ app.get('/signup', (req, res) => {
 });
 
 // Sample Blog Data
-const sampleBlogs = [
-    { title: 'First Blog Post', content: 'This is the content of the first blog post.' },
-    { title: 'Second Blog Post', content: 'This is the content of the second blog post.' }
-];
+const sampleBlogs = {
+    blogs: [
+        { image: 'https://i.pinimg.com/564x/80/c1/00/80c1009e693609f617bb29bed0707ed5.jpg', title: 'First Blog Post', content: 'This is the content of the first blog post.' },
+        { image: 'https://i.pinimg.com/564x/2c/ab/78/2cab78a8830951815f5f04a433cf4a4d.jpg', title: 'Second Blog Post', content: 'This is the content of the second blog post.' },
+        { image: 'https://i.pinimg.com/564x/b9/c3/48/b9c348c5822a243cb039b5da6b56a971.jpg', title: 'Third Blog Post', content: 'This is the content of the third blog post.' }
+    ]
+};
+
+const highlightSection = {
+    image: "https://i.pinimg.com/564x/44/b9/8e/44b98ea0ec307b62e4c6f85385190022.jpg",
+    title: "Innovative Solutions",
+    description: "Explore our cutting-edge technologies designed to solve real-world problems. From AI-driven platforms to IoT solutions, we are committed to innovation and excellence in every project we undertake."
+  };
+  
+
+
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
